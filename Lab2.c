15067ce217710ca69035ff0d9aa022171a71b31f
VIZIMAR <62505738+VIZIMAR@users.noreply.github.com>
2021-02-01 20:06:10 +0600

    Create Lab2.c

---
+++ Lab2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 20
+#define SIZE 256
+    char* s_gets(char*,int);
+    struct exam{
+        char name[LEN];
+        char surname[LEN];
+        int grade;
+    };
+int main(int argc,char**argv){
+    if(argc!=3){
+        perror("Error, uncorect count command arguments");
+        exit(EXIT_FAILURE);
+    }
+    FILE* fdIn,*fdOut;
+    int cnt,i=0;
+    char buf[SIZE];
+    ssize_t nRd;
+    printf("Input students count : ");
+    scanf(" %d",&cnt); // очистить входной поток
+    if (cnt==0)
+        exit(EXIT_FAILURE);
+        while(getchar()!='\n')
+            continue;
+    if((fdIn=fopen(argv[1],"w"))==NULL){
+            printf("%s ",argv[1]);
+            perror("Open error");
+            exit(EXIT_FAILURE); 
+            }
+    if((fdOut=fopen(argv[2],"w"))==NULL){
+            printf("%s ",argv[2]);
+            perror("Open error");
+            exit(EXIT_FAILURE); 
+            }        
+    struct exam students[cnt];
+    struct exam *ptr=&students[0];
+    printf("Enter student name (press \'\\n\' at the beginning to exit :");
+        while(i<cnt && s_gets((ptr+i)->name,LEN) && (ptr+i)->name[0]!='\0'){
+            fprintf(fdIn,"%s ",(ptr+i)->name); //fputs((ptr+i)->name,fdIn);
+            printf("Surname : ");
+            s_gets((ptr+i)->surname,LEN);
+            fprintf(fdIn,"%s ",(ptr+i)->surname); //fputs((ptr+i)->surname,fdIn);
+            printf("Grade : ");
+            scanf(" %d",&(ptr+i)->grade);
+            while(getchar()!='\n')
+            continue;
+            fprintf(fdIn," %d\n",(ptr+i)->grade);
+            if((ptr+i)->grade>70){
+                fprintf(fdOut,"%s ",(ptr+i)->name);
+                fprintf(fdOut,"%s ",(ptr+i)->surname);
+                fprintf(fdOut," %d\n",(ptr+i)->grade);
+            }
+            i++;
+            if(i<cnt)
+                printf("Enter student name (press \'\\n\' to exit :");
+            else
+                break;
+        }
+    return 0;
+}
+char* s_gets(char*st,int cnt){
+    char *find=fgets(st,cnt,stdin);
+    if(find){
+        find=strchr(st,'\n');
+        if(*find=='\n')
+            *find='\0';
+        else while(getchar()!='\n')
+            continue;        
+    } else return NULL;
+    return st; 
+}
